5749d944826d2604ff0bbd32b95fcb77723bfb53
Nícolas Brandão <107960652+nicolasbrandao@users.noreply.github.com>
2023-01-09 21:08:13 -0300

    Fixed navbar show/hide transitions

--- main.js
+++ main.js
@@ -3,31 +3,45 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#mode-icon').addEventListener('click', () => toggleMode())
 })
 
-function toggleMobileMenu() {
-    let closeURL = './static/assets/icons/close-menu-icon.svg'
-    let openURL ='./static/assets/icons/menu-icon.svg'
+function toggleMenuIcon () {
+    let closeURL = './static/assets/icons/close-menu-icon.svg';
+    let openURL ='./static/assets/icons/menu-icon.svg';
 
-    if ($("#nav-mobile-menu-container").css("display") === 'none') {
-        $('#nav-menu-icon').fadeOut(200, function(){
+    if (($('.nav-mobile-menu-container').is(":visible"))) {
+        $('#nav-menu-icon').fadeOut(100, function(){
             $(this).attr('src', closeURL).bind('onreadystatechange load', function(){
-               if (this.complete) $(this).fadeIn(200);
+               if (this.complete) $(this).fadeIn(100);
             });
         });
     } else {
-        $('#nav-menu-icon').fadeOut(200, function(){
+        $('#nav-menu-icon').fadeOut(100, function(){
             $(this).attr('src', openURL).bind('onreadystatechange load', function(){
-               if (this.complete) $(this).fadeIn(200);
+               if (this.complete) $(this).fadeIn(100);
             });
         });
-    }
+    };
+};
 
+function toggleMobileMenu() {
+    
     $('.nav-mobile-menu-container').slideToggle(300);
- 
-}
+    setTimeout(toggleMenuIcon,350);
+    
+};
+
+$(window).resize(function() {
+    if(window.innerWidth > 756) {
+        
+        if ($('.nav-mobile-menu-container').is(":visible")){
+            $('.nav-mobile-menu-container').slideUp();
+        }
+        toggleMenuIcon();
+    };
+});
 
 function toggleMode() {
-    let lightURL = './static/assets/icons/light-mode.svg'
-    let darkURL ='./static/assets/icons/dark-mode.svg'
+    let lightURL = './static/assets/icons/light-mode.svg';
+    let darkURL ='./static/assets/icons/dark-mode.svg';
 
     if ($('#mode-icon').attr('src') === darkURL) {
         $('#mode-icon').fadeOut(200, function(){
@@ -35,14 +49,13 @@ function toggleMode() {
                if (this.complete) $(this).fadeIn(200);
             });
         });
-        darkMode = false
+        darkMode = false;
     } else {
         $('#mode-icon').fadeOut(200, function(){
             $(this).attr('src', darkURL).bind('onreadystatechange load', function(){
                if (this.complete) $(this).fadeIn(200);
             });
         });
-        darkMode = true
-    }
-
-}
+        darkMode = true;
+    };
+};
